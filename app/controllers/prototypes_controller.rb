c3f1239121d81217b7ae148aa32466266554680b
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-09 18:18:39 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,57 @@
+class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    if user_signed_in?
+     @user = User.find(current_user.id)
+    end
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    if Prototype.create(prototype_params)
+      redirect_to root_path
+    else
+      render 'prototype/new'
+    end
+  end
+
+  def show 
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    if Prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render 'prototype/edit'
+    end
+  end
+  
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+   unless user_signed_in?
+     redirect_to action: :index
+   end
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    #@name = current_user.nickname
+    @prototypes = @user.prototypes
+
+  end
+end
