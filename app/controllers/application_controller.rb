d4cf6e1ebe6672befb31646ffce860a2608ab693
Yoneda3710 <71624609+Yoneda3710@users.noreply.github.com>
2020-10-12 19:00:59 +0900

    redirect_to の設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :move_to_index, except: [:index, :show]
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
   end
-
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
